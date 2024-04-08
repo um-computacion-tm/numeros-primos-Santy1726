3b7f7be0b4676b64089ab9ffdf44bbc9ece1c3e3
Santiago Ortega <sa.ortega@alumno.um.edu.ar>
2024-04-08 16:19:43 -0300

    Test numeros primos

--- testnumeros.py
+++ testnumeros.py
@@ -1,19 +1,6 @@
 from primos import is_primo
 import unittest
 
-def is_primo(value):
-    div = 2
-    if value == 1:
-        return False
-    while div < value:
-        if value % div == 0:
-            return False
-        div +=1
-    return True
-
-
-
-
 
 
 class TestsPrimos (unittest.TestCase):
